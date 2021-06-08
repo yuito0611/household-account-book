27275186966b8fa8d2ffc97f0c99ba3f88e6af4c
yuito0611 <yuito0611ohno@gmail.com>
2021-06-08 21:18:17 +0900

    base front-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import Data from './components/Data';
+import Container from 'react-bootstrap/Container';
+
+// react-bootstrap components
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+        <Row >
+          <Col></Col>
+
+          <Col sm={8} md={6}>
+            <Card>
+              <Card.Header><h1>家計簿</h1></Card.Header>
+              <Card.Body>
+                <Data />
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col></Col>
+        </Row>
+      </Container>
+
+
     </div>
   );
 }
